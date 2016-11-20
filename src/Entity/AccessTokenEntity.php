b797aab5411fedbe8a632d4915f88f7107d7ead0
phisch <git@philippschaffrath.de>
2016-11-20 19:53:10 +0100

    added some methods to access token entity

--- src/Entity/AccessTokenEntity.php
+++ src/Entity/AccessTokenEntity.php
@@ -4,5 +4,28 @@ namespace Phisch90\OAuth\Server\Entity;
 
 interface AccessTokenEntity
 {
+    /**
+     * @return string
+     */
+    public function getIdentifier();
 
+    /**
+     * @return ClientEntity
+     */
+    public function getClient();
+
+    /**
+     * @return UserEntity
+     */
+    public function getUser();
+
+    /**
+     * @return ScopeEntity[]
+     */
+    public function getScopes();
+
+    /**
+     * @return \DateTime
+     */
+    public function getExpiryDateTime();
 }
