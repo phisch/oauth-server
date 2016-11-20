4de057da6067a6e91265d4b80a4aa47693b2c868
phisch <git@philippschaffrath.de>
2016-11-20 20:54:40 +0100

    client entities should expose grant types and secret so it can be validated inside this library instead of a repository

--- src/Entity/ClientEntity.php
+++ src/Entity/ClientEntity.php
@@ -4,5 +4,13 @@ namespace Phisch90\OAuth\Server\Entity;
 
 interface ClientEntity
 {
+    /**
+     * @return array
+     */
+    public function getGrantTypes();
 
+    /**
+     * @return string
+     */
+    public function getSecret();
 }
