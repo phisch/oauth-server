78363f9442b88690cdcb1315561dde2fb35eb948
phisch <git@philippschaffrath.de>
2016-12-04 23:50:47 +0100

    added empty resource server

---
+++ src/ResourceServer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Phisch90\OAuth\Server;
+
+use Phisch90\OAuth\Server\Exception\AuthorizationServerException;
+use Phisch90\OAuth\Server\Grant\Grant;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ResourceServer
+{
+
+}
